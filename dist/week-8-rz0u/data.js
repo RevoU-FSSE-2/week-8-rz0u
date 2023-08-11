d9a878432477c26e6f1e25ba0d71100a513d565e
rz0u <rayhan.zou98@gmail.com>
2023-08-11 23:18:46 +0700

    fixed foldering issue

--- dist/week-8-rz0u/data.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.receipts = void 0;
-exports.receipts = [
-    { id: 1, type: "cash in", name: "gaji", detail: "gaji agustus", amount: 500.000 },
-    { id: 2, type: "cash out", name: "utang", detail: "jono minjem cepe", amount: 100.000 },
-    { id: 3, type: "cash in", name: "utang", detail: "jono bayar utang (nyicil)", amount: 20.000 },
-    { id: 4, type: "cash out", name: "slot", detail: "kalah main slot", amount: 400.000 }
-];
