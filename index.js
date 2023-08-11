229aa7b1fa595d9af84fc1799903a938ce2b5ea2
rz0u <rayhan.zou98@gmail.com>
2023-08-12 00:34:27 +0700

    deployment to fly.io

--- index.js
+++ index.js
@@ -1,115 +1,121 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // Environment -------------------------------------------------------------------
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
-import { receipts } from "./data";
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const data_1 = require("./data");
 // import jwt from "jsonwebtoken";
-
-dotenv.config();
-
-const app: Express = express();
+dotenv_1.default.config();
+const app = (0, express_1.default)();
 const port = process.env.port || 3000;
-
-app.use(bodyParser.json());
-app.use(express.json());
-
+app.use(body_parser_1.default.json());
+app.use(express_1.default.json());
 // Landing
-app.get("/", (req,res) => {
-    res.send("Financial Tracker Week 8, use /transactions endpoint")    
+app.get("/", (req, res) => {
+    res.send("Financial Tracker Week 8, use /transactions endpoint");
 });
-
 // Get all Transactions -------------------------------------------------------
-app.get("/transactions", (req,res) => {
+app.get("/transactions", (req, res) => {
     res.send({
         messages: "Here are all the transactions!",
-        receipts,
+        receipts: data_1.receipts,
     });
 });
-
 // Get Transactions by ID -----------------------------------------------------
-app.get("/transactions/:id", (req:Request,res:Response) => {
+app.get("/transactions/:id", (req, res) => {
     const receiptId = parseInt(req.params.id, 10);
-
     if (!Number.isNaN(receiptId)) {
-        let receipt = receipts.filter((item) => item.id === receiptId);
+        let receipt = data_1.receipts.filter((item) => item.id === receiptId);
         if (receipt.length !== 0) {
             res.json({
                 message: "Here is the transaction youre looking for!",
                 receipt,
             });
-        } else {
+        }
+        else {
             res.status(400).json({
                 message: "Failed to find the transaction :("
             });
         }
-    } else {
+    }
+    else {
         res.status(400).json({
             message: "Invalid transaction ID :("
         });
     }
 });
-
 // Post New Transaction(s) -----------------------------------------------------
-app.post("/transactions", (req,res) => {
+app.post("/transactions", (req, res) => {
     console.log(req.body);
-
     let receipt = {
-        id: receipts[receipts.length -1].id + 1,
+        id: data_1.receipts[data_1.receipts.length - 1].id + 1,
         type: req.body.type,
         name: req.body.name,
         detail: req.body.detail,
-        amount: req.body.amount,        
+        amount: req.body.amount,
     };
-
-    receipts.push(receipt);
-
+    data_1.receipts.push(receipt);
     res.json({
         message: "Successfully adding new transaction(s)!",
-        receipts,
+        receipts: data_1.receipts,
     });
 });
-
 // Put (Updates existing object with the given ID) -----------------------------
-app.put("/transactions/:id", (req,res) => {
+app.put("/transactions/:id", (req, res) => {
     const receiptId = parseInt(req.params.id, 10);
     if (!Number.isNaN(receiptId)) {
         let updatedTransaction = req.body;
-
-        const transactionIndex = receipts.findIndex((item) => item.id === receiptId);
-
+        const transactionIndex = data_1.receipts.findIndex((item) => item.id === receiptId);
         if (transactionIndex !== -1) {
-            receipts[transactionIndex] = {
-                ...updatedTransaction,
-                id: receiptId,
-            };
+            data_1.receipts[transactionIndex] = Object.assign(Object.assign({}, updatedTransaction), { id: receiptId });
             res.json({
                 message: "Transaction updated successfully!",
-                updatedTransaction: receipts[transactionIndex],
+                updatedTransaction: data_1.receipts[transactionIndex],
             });
-        } else {
+        }
+        else {
             res.status(400).json({
                 message: "Failed to find the transaction :("
             });
         }
-    } else {
+    }
+    else {
         res.status(400).json({
             message: "Invalid transaction ID :("
         });
     }
 });
-
 // // Patch (Partially updates existing object with the given ID) -------------------------------------
-app.patch("/transactions:id", (req,res) => {
-    const updateReceipt = receipts;
-    let success:boolean = false;
+app.patch("/transactions:id", (req, res) => {
+    const updateReceipt = data_1.receipts;
+    let success = false;
     for (let i = 0; i < updateReceipt.length; i++) {
         if (updateReceipt[i].id == +req.params.id) {
             const currReceipt = updateReceipt[i];
-            if (req.body.id) { updateReceipt[i].id = req.body.id};
-            if (req.body.type) { updateReceipt[i].type = req.body.type};
-            if (req.body.name) { updateReceipt[i].name = req.body.name};
-            if (req.body.detail) { updateReceipt[i].detail = req.body.detail};
-            if (req.body.amount) { updateReceipt[i].amount = req.body.amount};
+            if (req.body.id) {
+                updateReceipt[i].id = req.body.id;
+            }
+            ;
+            if (req.body.type) {
+                updateReceipt[i].type = req.body.type;
+            }
+            ;
+            if (req.body.name) {
+                updateReceipt[i].name = req.body.name;
+            }
+            ;
+            if (req.body.detail) {
+                updateReceipt[i].detail = req.body.detail;
+            }
+            ;
+            if (req.body.amount) {
+                updateReceipt[i].amount = req.body.amount;
+            }
+            ;
             success = true;
             res.json({
                 message: "Transaction updated successfully!", currReceipt
@@ -117,24 +123,19 @@ app.patch("/transactions:id", (req,res) => {
             console.log(req.body.id, req.body.type, req.body.detail);
         }
     }
-})
-
+});
 // app.patch("/transactions/:id", (req: Request, res: Response) => {
 //     const receiptId: number = parseInt(req.params.id, 10);
-
 //     if (!Number.isNaN(receiptId)) {
 //         const fieldToUpdate: keyof Receipt = req.body.field; // Request body contains field to update
 //         const updatedValue = req.body.value; // Updated value for the field
 //         console.log(req.body.field);
 //         console.log(req.body.value);
-
 //         // Find the transaction with the matching ID in the receipts array
 //         const transaction = receipts.find((item) => item.id === receiptId);
-
 //         if (transaction) {
 //             // Update the specified field with the new value
 //             (transaction as any)[fieldToUpdate] = updatedValue;
-
 //             res.json({
 //                 message: "Transaction updated successfully!",
 //                 updatedTransaction: transaction,
@@ -150,74 +151,65 @@ app.patch("/transactions:id", (req,res) => {
 //         });
 //     }
 // });
-
 // Delete (*ID*)
-app.delete("/transactions/:id", (req,res) => {
+app.delete("/transactions/:id", (req, res) => {
     const receiptId = parseInt(req.params.id, 10);
     if (!Number.isNaN(receiptId)) {
-        const transactionIndex = receipts.findIndex((item) => item.id === receiptId);
-        if(transactionIndex !== -1) {
-            const deletedTransaction = receipts.splice(transactionIndex, 1)[0];
-
+        const transactionIndex = data_1.receipts.findIndex((item) => item.id === receiptId);
+        if (transactionIndex !== -1) {
+            const deletedTransaction = data_1.receipts.splice(transactionIndex, 1)[0];
             res.json({
                 message: "Transaction deleted successfully!",
                 deletedTransaction,
             });
-        } else {
+        }
+        else {
             res.status(400).json({
                 message: "Failed to find the transaction :("
-            })
-        }     
-    } else {
+            });
+        }
+    }
+    else {
         res.status(400).json({
             message: "Invalid transaction ID :("
-        })
+        });
     }
-})
-
+});
 // Not Found
 app.get("*", (req, res) => {
     let body = `<h1>Error Page not found</h1>`;
     res.send(body);
-  });
-
+});
 // Port ----------------------------------------------------------------------
 app.listen(port, () => {
     console.log(`[server]: Server is running at localhost:${port}`);
 });
-
 // // User Data -------------------------------------------------------------------
 // interface User {
 //     username : string;
 //     password: string;
 //     role : string;
 // };
-
 // const users: User[] = [
 //     { username: "reovu", password: "pass123", role: "user"},
 //     { username: "donat", password: "123pass", role: "user"},
 //     { username: "dion", password: "123admin", role: "admin"},
 // ]
-
 // // Books --------------------------------------------------------------------
 // const books = [
 //     { name : "harry 1"},
 //     { name : "harry 2"},
 //     { name : "harry 3"},
 // ]
-
 // // Secret Access Token -------------------------------------------------------
 // const accessTokenSecret = process.env.secret || "secret";
-
 // // Function Login >> Bearer Token JWT ----------------------------------------
 // app.post("/login", (req,res) => {
 //     const {username, password} = req.body;
 //     console.log("body", username, password);
-
 //     const user = users.find((item) => {
 //         return item.username === username && item.password === password
 //     });
-
 //     if(user) {
 //         const accessToken = jwt.sign(
 //             {
@@ -234,12 +226,10 @@ app.listen(port, () => {
 //         res.send("Username or Password is Incorrect!");
 //     }
 // });
-
 // // Endpoint Books ( only Admin )------------------------------------------------
 // const verifyJWTAdmin = (req:any, res:Response, next:NextFunction) => {
 //     const authHeader = req.headers.authorization;
 //     console.log("cek auth header");
-
 //     if (authHeader) {
 //         const token = authHeader.split(" ")[1];
 //         jwt.verify(token, authHeader, accessTokenSecret, (error:any, user:any) =>{
@@ -248,7 +238,6 @@ app.listen(port, () => {
 //                 return res.status(401)
 //             }
 //             console.log("jwt okay", user);
-
 //             // Pass Data
 //             req.user = user as {username: string, role:string};
 //             next()
@@ -258,7 +247,6 @@ app.listen(port, () => {
 //         res.status(401);
 //     }
 // }
-
 // app.get("/books", verifyJWTAdmin, (req:any,res:Response) => {
 //     const { role } = 
 // })
